0161acccb7d2854b16f4e92606e8f5d0b3b6f05f
Laurence Finston <Laurence.Finston@gmx.de>
2021-04-18 16:19:29 +0200

    Added.

---
+++ OptiNum/Installer/dbsrvcli/dbsrvcli.c
@@ -0,0 +1,17 @@
+/* optinum.c  */
+/* Created by Laurence D. Finston (LDF) Wed Sep 23 13:27:41 CEST 2009  */
+
+/* Copyright (C) 2009 Gesellschaft fuer wissenschaftliche Datenverarbeitung mbH  */
+
+
+/* $Id: dbsrvcli.c 26 2009-12-04 14:23:07Z finston $  */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int
+main(int argc, char* argv[])
+{
+  return 0;
+}
